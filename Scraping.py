e0bc1acbc6dc50866c0a575029192b2329c32c13
valeriodipalo <82063143+valeriodipalo@users.noreply.github.com>
2022-09-20 18:28:57 +0200

    Web scraping - realized a new file. Just a doubt on how many stocks should be there

---
+++ Scraping.py
@@ -0,0 +1,17 @@
+# Here we will see scraping for exercise 3
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
+
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
+table = soup.find('table').find_all('tr')
+
+tickers = []
+for tr in table:
+  tickers.append(tr.find('a').text)
+
+tickers = tickers[1:]
+print(tickers)
+print((len(tickers)))
